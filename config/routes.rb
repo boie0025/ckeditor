8c1b6dc107e1207d4091fb2763a8eabbce1a4809
Ryan Francis <ryan.p.francis@gmail.com>
2014-11-01 23:48:58 -0500

    added photo albums

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Ckeditor::Engine.routes.draw do
   resources :attachment_files, :only => [:index, :create, :destroy]
   resources :folders
   resources :public_resources
+  resources :albums do
+    resources :content_assets, only: [:create, :destroy], controller: 'albums_content_assets'
+  end
 end
