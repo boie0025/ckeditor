abce387626b4da06ea70c7aaf1f8ce7a110b68a9
Ryan Francis <ryan.p.francis@gmail.com>
2015-03-09 17:49:48 -0500

    updated routes for /downloads path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Ckeditor::Engine.routes.draw do
   resources :pictures, :only => [:index, :create, :destroy]
   resources :attachment_files, :only => [:index, :create, :destroy]
   resources :folders
+  resources :public_resources, path: "/downloads"
   resources :public_resources
   resources :albums do
     resources :content_assets, only: [:create, :destroy], controller: 'albums_content_assets'
